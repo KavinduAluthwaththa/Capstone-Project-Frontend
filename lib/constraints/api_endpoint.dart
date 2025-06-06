7b8f2e9e32c51f89db1e749690c92a47006a5954
Kavindu Aluthwaththa <162307625+KavinduAluthwaththa@users.noreply.github.com>
2025-06-06 19:12:06 +0530

    Add crop prediction endpoint to ApiEndpoints

--- lib/constraints/api_endpoint.dart
+++ lib/constraints/api_endpoint.dart
@@ -94,4 +94,7 @@ class ApiEndpoints {
   // User
   static const String registerUser = "$baseUrl/User/Register";
   static const String loginUser = "$baseUrl/User/Login";
+
+  //suggestion model
+  static const String cropPrediction = "$baseUrl/CropRecommendation/predict"; //nitrogen, phosphorus,potassium,temperature,humidity,ph,rainfall
 }
